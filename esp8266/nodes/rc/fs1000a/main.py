e449ac9f3d89671954cd68ab3d6d01959b8a8bb5
Stefan Lehmann <Stefan.St.Lehmann@gmail.com>
2017-07-19 11:10:06 +0200

    fs1000a: fix error on automatic reset

--- esp8266/nodes/rc/fs1000a/main.py
+++ esp8266/nodes/rc/fs1000a/main.py
@@ -4,19 +4,23 @@ import ujson
 import credentials
 import core
 from rfsocket import RFSocket, Esp8266Timings
+import machine
 from machine import Pin, freq
 
 
 MQTT_CLIENT_ID = 'node2'
 REFRESH_TIME_MS = 100
 PINGTIME_S = 60
+SECONDS_TO_RESET = 60
 
 
 freq(160000000)
 rf_pin = Pin(0, Pin.OUT)
+mqtt = None
 
 
 def handle(topic, msg):
+    global mqtt
     payload = ujson.loads(msg)
     rf = RFSocket(rf_pin, RFSocket.ANSLUT, remote_id=payload['remote_id'],
                   timings=Esp8266Timings)
@@ -25,22 +29,42 @@ def handle(topic, msg):
     else:
         rf.off(payload['switch'])
 
+    if mqtt is not None:
+        mqtt.publish('home/power/out', msg)
+
 
 def run():
-    wifi = core.WifiWrapper(credentials.WIFI_SSID, credentials.WIFI_PASSWORD)
-    wifi.connect()
-    webrepl.start()
-    mqtt = core.MQTTClientWrapper(
-        client_id=MQTT_CLIENT_ID,
-        server=credentials.MQTT_SERVER,
-        port=credentials.MQTT_PORT,
-        user=credentials.MQTT_USER,
-        password=credentials.MQTT_PASSWORD,
-        ssl=credentials.MQTT_SSL,
-        keepalive=PINGTIME_S * 2
-    )
-    mqtt.connect()
-    mqtt.subscribe('home/power', handle)
+
+    global mqtt
+
+    try:
+        wifi = core.WifiWrapper(credentials.WIFI_SSID, credentials.WIFI_PASSWORD)
+        wifi.connect()
+    except WifiConnectionError:
+        machine.reset()
+
+
+    try:
+        webrepl.start()
+    except Exception as e:
+        print(e)
+
+    try:
+        mqtt = core.MQTTClientWrapper(
+            client_id=MQTT_CLIENT_ID,
+            server=credentials.MQTT_SERVER,
+            port=credentials.MQTT_PORT,
+            user=credentials.MQTT_USER,
+            password=credentials.MQTT_PASSWORD,
+            ssl=credentials.MQTT_SSL,
+            keepalive=PINGTIME_S * 2
+        )
+        mqtt.connect()
+        mqtt.subscribe('home/power/in', handle)
+    except core.MQTTConnectionError:
+        print('Could not connect to Mosquitto server. Performing reset in {} seconds.'.format(SECONDS_TO_RESET))
+        utime.sleep(SECONDS_TO_RESET)
+        machine.reset()
 
     ping_ticks_ms = utime.ticks_ms()
     while True:
@@ -53,7 +77,9 @@ def run():
                 ping_ticks_ms = utime.ticks_ms()
         except Exception as e:
             print(e)
+            print('Performing reset in {} seconds.'.format(SECONDS_TO_RESET))
+            utime.sleep(SECONDS_TO_RESET)
+            machine.reset()
 
         utime.sleep_ms(REFRESH_TIME_MS)
-
 
